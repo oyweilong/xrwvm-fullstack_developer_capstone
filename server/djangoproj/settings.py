00d92410d54c745d5afbcf65a8e7acd172161d40
oyweilong <158182348+oyweilong@users.noreply.github.com>
2024-05-30 00:15:48 +0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://oyweilong-8000.theiadockernext-1-labs-prod-theia
-                 k8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://oyweilong-8000.theiadockernext-1-labs-prod-theia \
+k8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://oyweilong-8000.theiadockernext-1-labs-prod-theia
-    k8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://oyweilong-8000.theiadockernext-1-labs-prod-theia \
+k8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
